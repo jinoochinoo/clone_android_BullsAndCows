b2096bfa9092a11373aaa7c4b0f80789d0f14602
jinoochinoo <73393147+jinoochinoo@users.noreply.github.com>
2021-02-15 22:15:39 +0900

    SoundPool 적용

--- app/src/main/java/com/example/numberbaseball/MainActivity.java
+++ app/src/main/java/com/example/numberbaseball/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.numberbaseball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,13 +21,20 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     int[] targetNumbers = new int[3];
+    int inputCount;
+    int hitCount;
 
     TextView[] inputArray = new TextView[3];
-    int inputCount;
     Button[] buttonArray = new Button[10];
 
     ImageButton backSpaceBtn;
     ImageButton hitButton;
+
+    TextView resultTextView;
+    ScrollView scrollView;
+
+    SoundPool soundPool;
+    int[] buttonSound = new int[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
         backSpaceBtn = findViewById(R.id.back_space_button);
         hitButton = findViewById(R.id.hit_button);
+        resultTextView = findViewById(R.id.resultTextView);
+        scrollView = findViewById(R.id.scroll_view);
 
         for(Button getButton : buttonArray){
             getButton.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
                         inputArray[inputCount].setText(button.getText().toString());
                         button.setEnabled(false);
                         inputCount++;
+                        
+                        // SoundPool 세팅
+                        soundPool.play(buttonSound[2], 1, 1, 1, 0, 1);
                     } else{
                         Toast.makeText(getApplicationContext(), "hit 버튼을 클릭하세요", Toast.LENGTH_SHORT).show();
                     }
@@ -69,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
                     // 입력된 값 삭제
                     inputArray[inputCount - 1].setText("");
                     inputCount--;
+                    // SoundPool 세팅
+                    soundPool.play(buttonSound[3], 1, 1, 1, 0, 1);
                 } else{
                     Toast.makeText(getApplicationContext(), "숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
                 }
@@ -88,6 +107,34 @@ public class MainActivity extends AppCompatActivity {
                     int[] countCheck = new int[2];
                     countCheck = getCountCheck(targetNumbers, userNumbers);
                     Log.e("hitButton", "countCheck ---- S : " + countCheck[0] + " B : " + countCheck[1]);
+
+                    String resultText = "";
+                    // 아웃
+                    if(countCheck[0] == 3){
+                        resultText = "1 [" + userNumbers[0] + " " + userNumbers[1] + " " +
+                                userNumbers[2] + " ]  아웃! 게임 끝! ";
+
+                        // SoundPool 세팅
+                        soundPool.play(buttonSound[0], 1, 1, 1, 0, 1);
+                    } else {
+                        resultText = "1 [" + userNumbers[0] + " " + userNumbers[1] + " " +
+                                userNumbers[2] + " ]  S : " + countCheck[0] + " / B : "
+                                + countCheck[1];
+                    } if(hitCount == 1){
+                            resultTextView.setText(resultText + "\n");
+                        } else{
+                            resultTextView.append(resultText + "\n");
+                        }
+
+                    if(countCheck[0] == 3){
+                        hitCount = 1;
+                        targetNumbers = getNumbers();
+                    } else{
+                        hitCount++;
+                    }
+
+                    // 포커스 내리기
+                    scrollView.fullScroll(View.FOCUS_DOWN);
 
                     // TextView, Button, inputCount 초기화
                     for(TextView textView : inputArray){
@@ -134,5 +181,31 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.e("setNumbers", "setNumbers = " + setNumbers[0] + ", " + setNumbers[1] + ", " + setNumbers[2]);
         return setNumbers;
+    }
+
+    // SoundPool, ButtonSound 세팅
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME).
+                            setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build();
+
+            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes)
+                    .setMaxStreams(6).build();
+        } else{
+            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        for (int i = 0; i < buttonSound.length; i++) {
+            buttonSound[i] = soundPool.load(getApplicationContext(), R.raw.button_1 + i, 1);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        soundPool.release();
     }
 }
